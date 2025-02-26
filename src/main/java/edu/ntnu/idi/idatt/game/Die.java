1e9abceaff56006cd5f2402f29882ef83ccf0af1
ingrop <ingop02@hotmail.com>
2025-02-26 22:40:27 +0100

    feat: die

--- src/main/java/edu/ntnu/idi/idatt/game/Die.java
+++ src/main/java/edu/ntnu/idi/idatt/game/Die.java
@@ -1,5 +1,35 @@
 package edu.ntnu.idi.idatt.game;
 
+import java.util.Random;
+
+/**
+ * Represents a single die with 6 sides.
+ */
 public class Die {
 
+  private static final Random random = new Random();
+  private static final int sides = 6;
+
+  private int value;
+
+
+  /**
+   * Rolls the die and gives it a random value between 1 and 6.
+   *
+   * @return the value of the die after rolling.
+   */
+  public int roll() {
+    this.value = random.nextInt(sides) + 1;
+    return this.value;
+  }
+
+
+  /**
+   * Returns the value of the die.
+   *
+   * @return the value of the die.
+   */
+  public int getValue() {
+    return this.value;
+  }
 }
