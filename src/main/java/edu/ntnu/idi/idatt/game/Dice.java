b2bce1765e6c71191f720a0505fdd976c8715b32
Aminda Lunde <aminda.lunde@gmail.com>
2025-03-05 13:33:53 +0100

    feat: roll method implemented

--- src/main/java/edu/ntnu/idi/idatt/game/Dice.java
+++ src/main/java/edu/ntnu/idi/idatt/game/Dice.java
@@ -2,15 +2,33 @@ package edu.ntnu.idi.idatt.game;
 
 import java.util.List;
 
+/**
+ *
+ */
 public class Dice {
 
   private Die[] dice;
 
+  /**
+   * @param diceAmount
+   */
   public Dice(int diceAmount) {
     dice = new Die[diceAmount];
     for (int i = 0; i < diceAmount; i++) {
       dice[i] = new Die();
     }
   }
+
+  /**
+   * @return
+   */
+  public int roll() {
+    int diceValue = 0;
+    for (Die d : dice) {
+      diceValue += d.roll();
+    }
+    return diceValue;
+  }
+
 
 }
