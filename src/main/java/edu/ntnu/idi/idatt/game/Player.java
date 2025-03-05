18e5ed16c9379edd423b3e5dfc9d9bb23699e907
ingrop <ingop02@hotmail.com>
2025-03-05 14:31:11 +0100

    chore: added javadoc

--- src/main/java/edu/ntnu/idi/idatt/game/Player.java
+++ src/main/java/edu/ntnu/idi/idatt/game/Player.java
@@ -1,18 +1,35 @@
 package edu.ntnu.idi.idatt.game;
 
+/**
+ * Represents a player in a board game.
+ */
 public class Player {
 
   private String name;
   Tile currentTile;
   BoardGame boardGame;
 
-  public Player(String name, BoardGame boardGame) {
+  /**
+   * Constructs a player with a name and a reference to the board game it is part of.
+   * Player should also know what tile it is currently on.
+   *
+   * @param name name of the player
+   * @param boardGame reference to which board game the player is part of
+   */
+  public Player(String name, BoardGame boardGame) {̉̉
     this.name = name;
     this.currentTile = null;
     this.boardGame = boardGame;
   }
 
+
+  /**
+   * Places the player on a certain tile.
+   */
   public void placeOnTile(Tile tile) {
     this.currentTile = tile;
   }
+
+  // TODO: Implement a move method that moves player x amount of steps
+
 }
