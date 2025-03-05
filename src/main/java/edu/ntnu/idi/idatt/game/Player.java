865fc60af8f3991b2cb5331d7215f514b97598b0
ingrop <ingop02@hotmail.com>
2025-03-05 13:46:07 +0100

    feat: Player constructor made

--- src/main/java/edu/ntnu/idi/idatt/game/Player.java
+++ src/main/java/edu/ntnu/idi/idatt/game/Player.java
@@ -2,4 +2,15 @@ package edu.ntnu.idi.idatt.game;
 
 public class Player {
 
+  private String name;
+  Tile currentTile;
+  BoardGame boardGame;
+
+  public Player(String name, BoardGame boardGame) {
+    this.name = name;
+    this.currentTile = null;
+    this.boardGame = boardGame;
+  }
+
+  
 }
