96d20bdb67781fbc8632634182f36126c4e844a4
Aminda Lunde <aminda.lunde@gmail.com>
2025-03-06 11:22:56 +0100

    refactor: improved DieTest naming and assertions

--- src/test/java/edu/ntnu/idi/idatt/DieTest.java
+++ src/test/java/edu/ntnu/idi/idatt/DieTest.java
@@ -16,30 +16,49 @@ class DieTest {
   }
 
   @Test
-  void roll_returnValueOneToSix() {
+  void roll_shouldReturnValueOneToSix() {
     int value = die.roll();
-    assertTrue(value >= 1 && value <= 6);
+    assertTrue(value >= 1 && value <= 6, "Rolled value out of range" + value);
   }
 
   @Test
-  void getValue_returnMostRecentValue() {
+  void getValue_shouldReturnMostRecentValue() {
     int rolledValue = die.roll();
     int valueFromGetValue = die.getValue();
-    assertEquals(valueFromGetValue, rolledValue);
+    assertEquals(valueFromGetValue, rolledValue, "getValue didn't return the most recent "
+        + "value");
 
   }
 
   @Test
   void getValue_shouldThrowExceptionIfNotRolled() {
-    assertThrows(IllegalStateException.class, () -> die.getValue());
+    assertThrows(IllegalStateException.class, () -> die.getValue(), "Expected "
+        + "IllegalStateException when getValue() is called before a roll");
   }
 
   @Test
   void roll_multipleRollsInRange() {
     for (int i = 0; i < 100; i++) {
       int rolledValue = die.roll();
-      assertTrue(rolledValue >= 1 && rolledValue <= 6);
+      assertTrue(rolledValue >= 1 && rolledValue <= 6, "Roll #" + i + " produced "
+          + "out of range value: " + rolledValue);
     }
+  }
+
+  @Test
+  void valueUpdatesOnRoll() {
+    die.roll();
+    int firstRoll = die.getValue();
+
+    die.roll();
+    int secondRoll = die.getValue();
+
+    assertTrue(firstRoll >= 1 && firstRoll <= 6, "First roll out of range: "
+        + firstRoll);
+    assertTrue(secondRoll >= 1 && secondRoll <= 6, "Second roll out of range: "
+        + secondRoll);
+
+    assertEquals(secondRoll, die.getValue(), "getValue didn't return the most recent roll");
   }
 
 }
