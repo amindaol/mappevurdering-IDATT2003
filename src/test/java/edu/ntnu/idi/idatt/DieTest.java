06ea06e853547e46f74325a2aeb23a092af500a6
Aminda Lunde <aminda.lunde@gmail.com>
2025-03-06 10:31:35 +0100

    feat: started on DieTest class

---
+++ src/test/java/edu/ntnu/idi/idatt/DieTest.java
@@ -0,0 +1,19 @@
+package edu.ntnu.idi.idatt;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DieTest {
+
+  @Test
+  void roll_returnValueOneToSix() {
+
+  }
+
+  @Test
+  void getValue_returnMostRecentValue() {
+
+  }
+
+}
