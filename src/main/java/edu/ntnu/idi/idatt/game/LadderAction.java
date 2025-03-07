1082e1b495c25b20fb6294f0532a4ff8b48b899b
Aminda Lunde <aminda.lunde@gmail.com>
2025-03-07 19:55:25 +0100

    feat: implemented methods in LadderAction

--- src/main/java/edu/ntnu/idi/idatt/game/LadderAction.java
+++ src/main/java/edu/ntnu/idi/idatt/game/LadderAction.java
@@ -1,5 +1,32 @@
 package edu.ntnu.idi.idatt.game;
 
-public class LadderAction {
+/**
+ *
+ */
+public class LadderAction implements TileAction {
 
+  private final int destinationTileId;
+  private final String description;
+
+  /**
+   * @param destinationTileId
+   * @param description
+   */
+  public LadderAction(int destinationTileId, String description) {
+    this.destinationTileId = destinationTileId;
+    this.description = description;
+  }
+
+  @Override
+  public void perform(Player player) {
+
+  }
+
+  public int getDestinationTileId() {
+    return destinationTileId;
+  }
+
+  public String getDescription() {
+    return description;
+  }
 }
