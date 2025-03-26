623ff53aecad5fe743e5a0e9d3ae993997d4c7fa
Aminda Lunde <aminda.lunde@gmail.com>
2025-03-26 13:19:45 +0100

    feat: Board methods

--- src/main/java/edu/ntnu/idi/idatt/game/Board.java
+++ src/main/java/edu/ntnu/idi/idatt/game/Board.java
@@ -1,5 +1,22 @@
 package edu.ntnu.idi.idatt.game;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+
+  private Map<Integer, Tile> tiles;
+
+  public Board() {
+    this.tiles = new HashMap<>();
+  }
+
+  public void addTile(Tile tile) {
+
+  }
+
+  public Tile getTile(int tileId) {
+
+  }
 
 }
