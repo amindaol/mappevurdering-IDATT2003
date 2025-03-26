8d51f0fe56a726f7f177c1346199d55fc284071a
Aminda Lunde <aminda.lunde@gmail.com>
2025-03-26 11:25:52 +0100

    feat: Tile constructor

--- src/main/java/edu/ntnu/idi/idatt/game/Tile.java
+++ src/main/java/edu/ntnu/idi/idatt/game/Tile.java
@@ -2,4 +2,13 @@ package edu.ntnu.idi.idatt.game;
 
 public class Tile {
 
+  private Tile nextTile;
+  private int tileId;
+  private TileAction landAction;
+
+  public Tile(int tileId) {
+    this.tileId = tileId;
+  }
+
+  
 }
