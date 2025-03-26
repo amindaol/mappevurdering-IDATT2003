c868b822b327cc750534426a1cba37579b989047
ingrop <ingop02@hotmail.com>
2025-03-26 18:50:28 +0100

    feat: methods for player

--- src/main/java/edu/ntnu/idi/idatt/game/Player.java
+++ src/main/java/edu/ntnu/idi/idatt/game/Player.java
@@ -1,5 +1,7 @@
 package edu.ntnu.idi.idatt.game;
 
+import java.util.stream.Stream;
+
 /**
  * Represents a player in a board game.
  */
@@ -7,29 +9,45 @@ public class Player {
 
   private String name;
   Tile currentTile;
-  BoardGame boardGame;
 
   /**
    * Constructs a player with a name and a reference to the board game it is part of.
    * Player should also know what tile it is currently on.
    *
    * @param name name of the player
-   * @param boardGame reference to which board game the player is part of
    */
-  public Player(String name, BoardGame boardGame) {̉̉
+  public Player(String name) {̉̉
     this.name = name;
     this.currentTile = null;
-    this.boardGame = boardGame;
   }
 
 
   /**
    * Places the player on a certain tile.
+   *
+   * @param tile new tile for the player to be placed on
    */
   public void placeOnTile(Tile tile) {
     this.currentTile = tile;
   }
 
-  // TODO: Implement a move method that moves player x amount of steps
 
+
+  public void move(int steps) {
+    
+  }
+
+
+  /**
+   * Returns the tile the player is currently on
+   *
+   * @return current tile
+   */
+  public Tile getCurrentTile() {
+    return currentTile;
+  }
+
+  public String getName() {
+    return name;
+  }
 }
