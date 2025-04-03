c404cd65b288036fbbaa3def0272bbe6939965d8
Aminda Lunde <aminda.lunde@gmail.com>
2025-04-03 14:44:51 +0200

    chore: javaDoc for BoardGame

--- src/main/java/edu/ntnu/idi/idatt/game/BoardGame.java
+++ src/main/java/edu/ntnu/idi/idatt/game/BoardGame.java
@@ -16,21 +16,24 @@ public class BoardGame {
   private Dice dice;
 
   /**
-   * Constructs a
+   * Constructs a new BoardGame instance with an empty player list.
    */
   public BoardGame() {
     this.players = new ArrayList<>();
   }
 
   /**
-   * @param player
+   * Adds a player to the game.
+   *
+   * @param player the player to add to the game.
    */
   public void addPlayer(Player player) {
     players.add(player);
   }
 
   /**
-   *
+   * Initializes the game board with a set number of tiles. Currently creates 30 tiles and adds them
+   * to the board.
    */
   public void createBoard() {
     this.board = new Board();
@@ -41,42 +44,53 @@ public class BoardGame {
   }
 
   /**
-   *
+   * Initialized the dice used in the game. Creates a Dice object with 1 die.
    */
   public void createDice() {
     this.dice = new Dice(1);
   }
 
   /**
-   *
+   * Starts the game loop.
+   * <p>
+   * This method should contain the main logic for playing turns, rolling dice, and determining the
+   * winner. Currently, it just prints a message.
    */
   public void play() {
     System.out.println("Game started with " + players.size() + " players");
   }
 
   /**
-   * @return
+   * Returns the winner of the game.
+   *
+   * @return the winning player, or null if the game is not finished.
    */
   public Player getWinner() {
     return null;
   }
 
   /**
-   * @return
+   * Returns the game board.
+   *
+   * @return the Board used in the game.
    */
   public Board getBoard() {
     return board;
   }
 
   /**
-   * @return
+   * Returns the dice used in the game.
+   *
+   * @return the Dice instance.
    */
   public Dice getDice() {
     return dice;
   }
 
   /**
-   * @return
+   * Returns the list of players in the game.
+   *
+   * @return a list of Player objects.
    */
   public List<Player> getPlayers() {
     return players;
