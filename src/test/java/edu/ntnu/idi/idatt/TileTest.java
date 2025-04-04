ddb4445050f6811d89a10bdae85580da6dcf248f
Aminda Lunde <aminda.lunde@gmail.com>
2025-04-04 14:03:10 +0200

    feat: setUp in TileTest

--- src/test/java/edu/ntnu/idi/idatt/TileTest.java
+++ src/test/java/edu/ntnu/idi/idatt/TileTest.java
@@ -1,5 +1,23 @@
 package edu.ntnu.idi.idatt;
 
-public class TileTest {
+import edu.ntnu.idi.idatt.game.BoardGame;
+import edu.ntnu.idi.idatt.game.Player;
+import edu.ntnu.idi.idatt.game.Tile;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+class TileTest {
+
+  private Tile tile;
+  private Player player;
+  private BoardGame game;
+
+  @BeforeEach
+  void setUp() {
+    game = new BoardGame();
+    tile = new Tile(1);
+    player = new Player("Test player", game);
+  }
 }
