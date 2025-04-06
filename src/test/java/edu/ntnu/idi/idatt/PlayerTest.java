a21cfd5d5aa698b2fbebe76ef55671af3a5bef4a
Aminda Lunde <aminda.lunde@gmail.com>
2025-04-06 16:58:56 +0200

    test: Player class

---
+++ src/test/java/edu/ntnu/idi/idatt/PlayerTest.java
@@ -0,0 +1,53 @@
+package edu.ntnu.idi.idatt;
+
+import edu.ntnu.idi.idatt.game.BoardGame;
+import edu.ntnu.idi.idatt.game.Player;
+import edu.ntnu.idi.idatt.game.Tile;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+  private Player player;
+  private BoardGame boardGame;
+  private Tile startTile;
+
+  @BeforeEach
+  void setUp() {
+    boardGame = new BoardGame();
+    boardGame.createBoard();
+    player = new Player("Test Player", boardGame);
+    startTile = boardGame.getBoard().getTile(1);
+  }
+
+  @Test
+  void constructor_setsName() {
+    assertEquals("Test Player", player.getName());
+  }
+
+  @Test
+  void testPlaceOnTile() {
+    player.placeOnTile(startTile);
+    assertEquals(startTile, player.getCurrentTile());
+  }
+
+  @Test
+  void movesCorrectNumberOfSteps() {
+    player.placeOnTile(startTile);
+    startTile.setNextTile(new Tile(2));
+    player.move(1);
+    assertEquals(2, player.getCurrentTile().getTileId());
+  }
+
+  @Test
+  void move_withoutTile_throwsException() {
+    assertThrows(IllegalStateException.class, () -> player.move(1));
+  }
+
+  @Test
+  void getBoardGame_returnsCorrectBoardGame() {
+    assertEquals(boardGame, player.getBoardGame());
+  }
+}
