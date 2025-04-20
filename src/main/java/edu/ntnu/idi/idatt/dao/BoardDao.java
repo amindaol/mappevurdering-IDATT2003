f64db9376f1ca1602fdadf90a56f50418c5f4c31
Aminda Lunde <aminda.lunde@gmail.com>
2025-04-20 14:36:38 +0200

    chore; javadoc for BoardDao

--- src/main/java/edu/ntnu/idi/idatt/dao/BoardDao.java
+++ src/main/java/edu/ntnu/idi/idatt/dao/BoardDao.java
@@ -7,8 +7,20 @@ import edu.ntnu.idi.idatt.game.Board;
  */
 public interface BoardDao {
 
+  /**
+   * Reads a Board configuration from persistent storage.
+   *
+   * @return the loaded Board instance
+   * @throws DaoException if an I/O or parsing error occurs
+   */
   Board readBoard() throws DaoException;
 
+  /**
+   * Writes the given Board configuration to persistent storage.
+   *
+   * @param board the Board to serialize
+   * @throws DaoException if an I/O error occurs during writing
+   */
   void writeBoard(Board board) throws DaoException;
 
 }
