426a4c5b8c0144c6d65a7943a48df338394a682d
ingrop <ingop02@hotmail.com>
2025-04-29 10:48:15 +0200

    feat(Main): initialized UiController

--- src/main/java/edu/ntnu/idi/idatt/Main.java
+++ src/main/java/edu/ntnu/idi/idatt/Main.java
@@ -1,10 +1,20 @@
 package edu.ntnu.idi.idatt;
 
 import javafx.application.Application;
+import javafx.stage.Stage;
+import main.java.edu.ntnu.idi.idatt.view.UiController;
 
 public class Main extends Application {
 
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    UiController controller = new UiController();
+    controller.showHomePage();
+
+    primaryStage.show();
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    launch(args);
   }
 }
