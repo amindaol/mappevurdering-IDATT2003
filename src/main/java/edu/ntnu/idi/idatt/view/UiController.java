47938a2f438dd1918be6116910f56b0ddc467151
ingrop <ingop02@hotmail.com>
2025-04-29 10:19:31 +0200

    feat(ui): UiController class made

---
+++ src/main/java/edu/ntnu/idi/idatt/view/UiController.java
@@ -0,0 +1,11 @@
+package main.java.edu.ntnu.idi.idatt.view;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class UiController {
+
+  private final Stage stage;
+  
+
+}
---
+++ src/main/java/edu/ntnu/idi/idatt/view/home/HomePage.java
@@ -0,0 +1,5 @@
+package main.java.edu.ntnu.idi.idatt.view.home;
+
+public class HomePage {
+
+}
