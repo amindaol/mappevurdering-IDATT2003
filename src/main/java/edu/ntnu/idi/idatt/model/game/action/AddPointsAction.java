821b50e977a992ec3282fea720fbbfc9132cfc00
Aminda Lunde <aminda.lunde@gmail.com>
2025-05-08 11:24:10 +0200

    feat: Added pointsAction classes and refactored player class

---
+++ src/main/java/edu/ntnu/idi/idatt/model/game/action/AddPointsAction.java
@@ -0,0 +1,23 @@
+package edu.ntnu.idi.idatt.model.game.action;
+
+import edu.ntnu.idi.idatt.model.game.Player;
+
+public class AddPointsAction implements TileAction {
+  private final int points;
+
+  public AddPointsAction(int points) {
+    if (points < 0) throw new IllegalArgumentException("Points must be positive");
+    this.points = points;
+  }
+
+  @Override
+  public void perform(Player player) {
+    if (player == null) throw new NullPointerException("Player cannot be null");
+    player.addPoints(points);
+  }
+
+  public int getPoints() {
+    return points;
+  }
+}
+
