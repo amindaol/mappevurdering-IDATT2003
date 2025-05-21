6278b0c49fddfd59c17c7d1c7773f3b257eda8dc
ingrop <ingop02@hotmail.com>
2025-05-21 20:46:46 +0200

    chore: Added JavaDoc

--- src/main/java/edu/ntnu/idi/idatt/observer/BoardGameEvent.java
+++ src/main/java/edu/ntnu/idi/idatt/observer/BoardGameEvent.java
@@ -1,5 +1,9 @@
 package edu.ntnu.idi.idatt.observer;
 
+/**
+ * Enum representing the different events that can occur in a board game. This enum is used to
+ * notify observers about specific game events.
+ */
 public enum BoardGameEvent {
   GAME_START,
   DICE_ROLLED,
